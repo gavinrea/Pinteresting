51267da8c3ef844ea8d833bed31461662a2c5270
Gavin Rea <gavin@reaworld.org>
2014-10-28 22:17:38 -0700

    this

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,8 @@ class Pin < ActiveRecord::Base
 	# added for paperclip gem
 	#styles will create two versions, a medium and thumbnail:
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	# validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 
 
 	validates :image, presence: true
